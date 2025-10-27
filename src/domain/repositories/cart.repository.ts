33157cb6e9869147131d52b6d10eff8f618bfea3
shafeeque <shafeeqsha06@gmail.com>
2025-10-27 12:28:27 +0530

    feat(cart): add cart database repository implmentation

---
+++ src/domain/repositories/cart.repository.ts
@@ -0,0 +1,20 @@
+import { CartItem } from "../entities/cart-item.entity";
+import { Cart } from "../entities/cart.entity";
+
+export abstract class ICartRepository {
+  abstract create(cart: Cart): Promise<void>;
+  abstract delete(cart: Cart): Promise<void>;
+  abstract findById(id: string): Promise<Cart | null>;
+  abstract findItemByUserIdAndCourseId(
+    userId: string,
+    courseId: string
+  ): Promise<CartItem | null>;
+  abstract findByUserId(
+    userId: string,
+    offset?: number,
+    limit?: number
+  ): Promise<{ cart: Cart | null; totalItems: number }>;
+  abstract update(cart: Cart): Promise<void>;
+  abstract addItem(cartItem: CartItem): Promise<void>;
+  abstract removeItem(cartId: string, courseId: string): Promise<void>;
+}
