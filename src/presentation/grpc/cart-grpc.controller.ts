b33e5f4a249d7590287a97731cc163f6127968d1
shafeeque <shafeeqsha06@gmail.com>
2026-02-06 20:57:52 +0530

    feat(controller): update grpc cart controller

--- src/presentation/grpc/cart-grpc.controller.ts
+++ src/presentation/grpc/cart-grpc.controller.ts
@@ -4,31 +4,35 @@ import { LoggingService } from "src/infrastructure/observability/logging/logging
 import { TracingService } from "src/infrastructure/observability/tracing/trace.service";
 import { DomainException } from "src/domain/exceptions/domain.exceptions";
 import {
-  Error,
-  PaginationResponse,
   AddToCartRequest,
   AddToCartResponse,
   RemoveFromCartRequest,
   RemoveFromCartResponse,
-  CartData,
-  CartItemData,
   ListCartRequest,
   ListCartResponse,
   ToggleCartItemRequest,
   ToggleCartItemResponse,
-} from "src/infrastructure/grpc/generated/user_service";
+  ClearCartRequest,
+  ClearCartResponse,
+} from "src/infrastructure/grpc/generated/user/types/cart_types";
 import { AddToCartUseCase } from "src/application/use-cases/cart/add-to-cart.use-case";
 import { RemoveFromCartUseCase } from "src/application/use-cases/cart/remove-cart.use-case";
 import { GetCartByUserUseCase } from "src/application/use-cases/cart/get-cart-by-user.use-case";
 import { Cart } from "src/domain/entities/cart.entity";
 import { CartItem } from "src/domain/entities/cart-item.entity";
 import { ToggleCartUseCase } from "src/application/use-cases/cart/toggle-cart.use-case";
+import {
+  Error,
+  PaginationResponse,
+} from "src/infrastructure/grpc/generated/user/common";
+import { ClearCartUseCase } from "src/application/use-cases/cart/clear-cart.use-case";
 
 @Controller()
 export class CartGrpcController {
   constructor(
     private readonly addToCartUseCase: AddToCartUseCase,
     private readonly removeFromCartUseCase: RemoveFromCartUseCase,
+    private readonly clearCartUseCase: ClearCartUseCase,
     private readonly toggleCartItemUseCase: ToggleCartUseCase,
     private readonly getCartByUserUseCase: GetCartByUserUseCase,
 
@@ -40,17 +44,20 @@ export class CartGrpcController {
     return {
       code: error.errorCode,
       message: error.message,
-      details: error?.serializeError(),
+      details:
+        "serializeError" in error && typeof error.serializeError === "function"
+          ? error.serializeError()
+          : [{ message: error.message }],
     };
   }
 
-  @GrpcMethod("UserService", "AddToCart")
+  @GrpcMethod("CartService", "AddToCart")
   async addToCart(data: AddToCartRequest): Promise<AddToCartResponse> {
     try {
       return await this.tracer.startActiveSpan(
         "CartGrpcController.addToCart",
         async (span) => {
-          const { courseId, userId, cartId } = data!;
+          const { courseId, userId } = data!;
 
           span.setAttributes({ courseId, userId });
           this.logger.info("Handling `AddToCart` request ", {
@@ -58,7 +65,6 @@ export class CartGrpcController {
           });
 
           const cartItem = await this.addToCartUseCase.execute(
-            cartId,
             userId,
             courseId
           );
@@ -66,7 +72,7 @@ export class CartGrpcController {
           this.logger.info("AddToCart request has been successfully completed");
 
           return {
-            item: this.mapToCartItemResponse(cartItem),
+            item: cartItem.toGrpcResponse(),
           };
         }
       );
@@ -78,13 +84,15 @@ export class CartGrpcController {
     }
   }
 
-  @GrpcMethod("UserService", "ToggleCartItem")
-  async toggleCart(data: ToggleCartItemRequest): Promise<ToggleCartItemResponse> {
+  @GrpcMethod("CartService", "ToggleCartItem")
+  async toggleCart(
+    data: ToggleCartItemRequest
+  ): Promise<ToggleCartItemResponse> {
     try {
       return await this.tracer.startActiveSpan(
         "CartGrpcController.ToggleCartItem",
         async (span) => {
-          const { courseId, userId, cartId } = data!;
+          const { courseId, userId } = data!;
 
           span.setAttributes({ courseId, userId });
           this.logger.info("Handling `ToggleCartItem` request ", {
@@ -92,15 +100,16 @@ export class CartGrpcController {
           });
 
           const cartItem = await this.toggleCartItemUseCase.execute(
-            cartId,
             userId,
             courseId
           );
 
-          this.logger.info("ToggleCartItem request has been successfully completed");
+          this.logger.info(
+            "ToggleCartItem request has been successfully completed"
+          );
 
           return {
-            item: this.mapToCartItemResponse(cartItem),
+            item: cartItem.toGrpcResponse(),
           };
         }
       );
@@ -111,7 +120,7 @@ export class CartGrpcController {
       return { error: this.createErrorResponse(error) };
     }
   }
-  @GrpcMethod("UserService", "RemoveFromCart")
+  @GrpcMethod("CartService", "RemoveFromCart")
   async removeFromCart(
     data: RemoveFromCartRequest
   ): Promise<RemoveFromCartResponse> {
@@ -119,15 +128,15 @@ export class CartGrpcController {
       return await this.tracer.startActiveSpan(
         "CartGrpcController.removeFromCart",
         async (span) => {
-          const { courseId, cartId } = data!;
+          const { courseId, userId } = data!;
 
-          span.setAttributes({ courseId, cartId });
+          span.setAttributes({ courseId, userId });
           this.logger.info("Handling `RemoveFromCart` request ", {
             ctx: CartGrpcController.name,
           });
 
           const cart = await this.removeFromCartUseCase.execute(
-            cartId,
+            userId,
             courseId
           );
 
@@ -147,7 +156,36 @@ export class CartGrpcController {
       return { error: this.createErrorResponse(error) };
     }
   }
-  @GrpcMethod("UserService", "ListUserCart")
+  @GrpcMethod("CartService", "ClearCart")
+  async clearCart(data: ClearCartRequest): Promise<ClearCartResponse> {
+    try {
+      return await this.tracer.startActiveSpan(
+        "CartGrpcController.clearCart",
+        async (span) => {
+          const { userId } = data!;
+
+          span.setAttributes({ userId });
+          this.logger.info("Handling `clearCart` request ", {
+            ctx: CartGrpcController.name,
+          });
+
+          const cart = await this.clearCartUseCase.execute(userId);
+
+          this.logger.info("ClearCart request has been successfully completed");
+
+          return {
+            success: { removed: true },
+          };
+        }
+      );
+    } catch (error) {
+      this.logger.error("Error processing gRPC request `RemoveFromCart`", {
+        error,
+      });
+      return { error: this.createErrorResponse(error) };
+    }
+  }
+  @GrpcMethod("CartService", "ListUserCart")
   async listUserCart(data: ListCartRequest): Promise<ListCartResponse> {
     try {
       return await this.tracer.startActiveSpan(
@@ -160,24 +198,22 @@ export class CartGrpcController {
             ctx: CartGrpcController.name,
           });
 
-          const { cart, total } =
-            await this.getCartByUserUseCase.execute(
-              userId,
-              pagination.page,
-              pagination.pageSize
-            );
+          const { cart, total } = await this.getCartByUserUseCase.execute(
+            userId,
+            pagination.page,
+            pagination.pageSize
+          );
 
           this.logger.info(
             "ListUserCart request has been successfully completed"
           );
           const paginationResponse: PaginationResponse = {
             totalItems: total, // Replace with actual total items if available
-            totalPages: Math.ceil(total / pagination.pageSize), // Replace with actual total pages if available
           };
 
           return {
             success: {
-              cart: this.mapToCartResponse(cart),
+              cart: cart.toGrpcResponse(),
               pagination: paginationResponse,
             },
           };
@@ -191,24 +227,24 @@ export class CartGrpcController {
     }
   }
 
-  private mapToCartItemResponse(cartItem: CartItem): CartItemData {
-    if (!cartItem) return;
-    return {
-      courseId: cartItem.getCourseId(),
-      createdAt: cartItem.getAddedAt()?.toISOString(),
-      id: cartItem.getId(),
-    };
-  }
+  // private mapToCartItemResponse(cartItem: CartItem): CartItemData {
+  //   if (!cartItem) return;
+  //   return {
+  //     courseId: cartItem.getCourseId(),
+  //     createdAt: cartItem.getAddedAt()?.toISOString(),
+  //     id: cartItem.getId(),
+  //   };
+  // }
 
-  private mapToCartResponse(cart: Cart): CartData {
-    if (!cart) return;
-    return {
-      createdAt: cart.getCreatedAt()?.toISOString(),
-      id: cart.getId(),
-      userId: cart.getUserId(),
-      items: cart.getItems()?.map(this.mapToCartItemResponse),
-      total: cart.getItems().length,
-      updatedAt: cart.getUpdatedAt()?.toISOString(),
-    };
-  }
+  // private mapToCartResponse(cart: Cart): CartData {
+  //   if (!cart) return;
+  //   return {
+  //     createdAt: cart.getCreatedAt()?.toISOString(),
+  //     id: cart.getId(),
+  //     userId: cart.getUserId(),
+  //     items: cart.getItems()?.map(this.mapToCartItemResponse),
+  //     total: cart.getItems().length,
+  //     updatedAt: cart.getUpdatedAt()?.toISOString(),
+  //   };
+  // }
 }
