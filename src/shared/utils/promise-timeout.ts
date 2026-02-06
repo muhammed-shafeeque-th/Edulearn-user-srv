9adae811cf1cb290bf3c8c4c918f3a3d77c3f947
shafeeque <shafeeqsha06@gmail.com>
2026-02-06 20:26:11 +0530

    feat(promise-timout): add utility function to for implement timout for interservice communications

---
+++ src/shared/utils/promise-timeout.ts
@@ -0,0 +1,27 @@
+import { TimeoutException } from 'src/domain/exceptions/domain.exceptions';
+
+export function promiseTimeout<T>(
+  callback: () => Promise<T>,
+  message: string = 'Promise callback timed out',
+  timeout: number = 10000,
+): Promise<T> {
+  let timer: NodeJS.Timeout;
+  return new Promise<T>((resolve, reject) => {
+    // Start the timeout timer
+    timer = setTimeout(() => {
+      reject(new TimeoutException(message));
+    }, timeout);
+
+    // Run the callback
+    Promise.resolve()
+      .then(callback)
+      .then((value) => {
+        clearTimeout(timer);
+        resolve(value);
+      })
+      .catch((err) => {
+        clearTimeout(timer);
+        reject(err);
+      });
+  });
+}
