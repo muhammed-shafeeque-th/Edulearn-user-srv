2dd262c63d9375240941e8847ab409d8c04b0425
shafeeque <shafeeqsha06@gmail.com>
2026-02-07 16:55:48 +0530

    feat(entity): add domain wishlist entity

---
+++ src/domain/repositories/wishlist.repository.ts
@@ -0,0 +1,20 @@
+import { WishlistItem } from "../entities/wishlist-item.entity";
+import { Wishlist } from "../entities/wishlist.entity";
+
+export abstract class IWishlistRepository {
+  abstract create(wishlist: Wishlist): Promise<void>;
+  abstract delete(wishlist: Wishlist): Promise<void>;
+  abstract findById(id: string): Promise<Wishlist | null>;
+  abstract findItemByUserIdAndCourseId(
+    userId: string,
+    courseId: string
+  ): Promise<WishlistItem | null>;
+  abstract findByUserId(
+    userId: string,
+    offset?: number,
+    limit?: number
+  ): Promise<{ wishlist: Wishlist | null; totalItems: number }>;
+  abstract update(wishlist: Wishlist): Promise<void>;
+  abstract addItem(wishlistItem: WishlistItem): Promise<void>;
+  abstract removeItem(wishlistId: string, courseId: string): Promise<void>;
+}
