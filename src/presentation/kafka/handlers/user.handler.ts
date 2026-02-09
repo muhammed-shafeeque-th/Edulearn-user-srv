3296c8b71e01d4c3058d985003f5ceb873e8228e
shafeeque <shafeeqsha06@gmail.com>
2026-02-09 08:29:10 +0530

    feat(kafka): add kafka event handler for user events

---
+++ src/presentation/kafka/handlers/user.handler.ts
@@ -0,0 +1,51 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import CreateUserUseCase from 'src/application/use-cases/user/create-user.use-case';
+import { USER_EVENT_TYPES, UserCreatedEvent } from 'src/domain/events/user-created.event';
+import { IEventProcessRepository } from 'src/domain/repositories/event-process-repository.interface';
+import { LoggingService } from 'src/infrastructure/observability/logging/logging.service';
+import { KafkaTopics } from 'src/shared/events';
+
+@Injectable()
+export class UserHandler {
+  constructor(
+    private readonly eventProcessRepository: IEventProcessRepository,
+    private readonly userCreatedUseCase: CreateUserUseCase,
+    private readonly logger: LoggingService,
+  ) {}
+
+  async handle(raw: UserCreatedEvent) {
+    const event = raw as UserCreatedEvent;
+    let alreadyProcessed: boolean;
+    try {
+      alreadyProcessed = await this.eventProcessRepository.isProcessed(
+        event.eventId
+      );
+    } catch (err) {
+      this.logger.error(
+        `Error checking event process repository for eventId ${event.eventId}: ${err?.message}`,
+        err?.stack
+      );
+      throw new InternalServerErrorException(
+        "Could not verify event processing state"
+      );
+    }
+    if (alreadyProcessed) {
+      this.logger.debug(
+        `[Event Already Processed] Skipping: ${event.eventId}`,
+        { ctx: "CreateEnrollmentFromOrderUseCase" }
+      );
+      return;
+    }
+
+    // if (event.eventType === USER_EVENT_TYPES.CREATED) {
+
+      await this.userCreatedUseCase.execute(event);
+
+      await this.eventProcessRepository.markAsProcessed(event.eventId);
+
+        this.logger.info(
+          `Successfully processed order enrollment for topic ${KafkaTopics.CourseEnrollmentCreated}`
+        );
+    // }
+  }
+}
