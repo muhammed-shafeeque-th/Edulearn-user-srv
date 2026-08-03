d73e7955ed8262e8afc88dc4dc24a2560bde89b5
shafeeque <shafeeqsha06@gmail.com>
2026-08-03 21:47:51 +0530

    chore: update gitignore file and eslint configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
