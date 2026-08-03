2b3a7776ee7d1c139b8e66a3935beb655bf113f3
shafeeque <shafeeqsha06@gmail.com>
2026-08-03 21:32:44 +0530

    feat(kafka): udpate kafak event handlers, depend on abstract services, and fixed lint issues

--- src/presentation/kafka/consumers/course.consumer.ts
+++ src/presentation/kafka/consumers/course.consumer.ts
@@ -1,13 +1,16 @@
 import { Controller } from "@nestjs/common";
 
-import { LoggingService } from "src/infrastructure/observability/logging/logging.service";
-import { TracingService } from "src/infrastructure/observability/tracing/trace.service";
+import { ILoggerService } from "src/application/adaptors/logger.service";
+import { ITraceService } from "src/application/adaptors/trace.service";
 import { KafkaTopics } from "src/shared/events";
-import { KafkaMessage } from "src/infrastructure/kafka/kafka.types";
-import { CourseCreatedEvent, CourseEnrollmentEvent } from "src/domain/events/course.events";
+import { KafkaMessage } from "@/infrastructure/kafka/module/kafka.types";
+import {
+  CourseCreatedEvent,
+  CourseEnrollmentEvent,
+} from "src/domain/events/course.events";
 import { CourseEnrolledHandler } from "../handlers/course-enrolled.handler";
 import { CourseCreatedHandler } from "../handlers/course-created.handler";
-import { EventPattern } from "src/infrastructure/kafka/kafka.decorators";
+import { EventPattern } from "@/infrastructure/kafka/module/kafka.decorators";
 
 @Controller()
 export class CourseConsumer {
@@ -15,69 +18,70 @@ export class CourseConsumer {
     private readonly courseEnrolledHandler: CourseEnrolledHandler,
     private readonly courseCreatedHandler: CourseCreatedHandler,
 
-    private readonly tracer: TracingService,
-    private readonly logger: LoggingService
-  ) { }
+    private readonly _logger: ILoggerService,
+    private readonly _tracer: ITraceService,
+  ) {}
 
-   
   @EventPattern(KafkaTopics.CourseCreated)
   async handleCourseCreateEvent(
-    data: KafkaMessage<CourseCreatedEvent>
+    data: KafkaMessage<CourseCreatedEvent>,
   ): Promise<void> {
     try {
-      await this.tracer.startActiveSpan(
+      await this._tracer.startActiveSpan(
         "CourseConsumer.handleCourseComplete",
         async () => {
-          this.logger.debug("Received data : " + JSON.stringify(data, null, 2));
-          this.logger.info("Handling `handleCourseComplete` event handler ", {
+          this._logger.debug(
+            "Received data : " + JSON.stringify(data, null, 2),
+          );
+          this._logger.debug("Handling `handleCourseComplete` event handler ", {
             ctx: CourseConsumer.name,
-          });    
+          });
 
           await this.courseCreatedHandler.handle(data.value);
 
-          this.logger.info(
-            "handleCourseComplete event handle has been successfully completed"
+          this._logger.debug(
+            "handleCourseComplete event handle has been successfully completed",
           );
-
-        }
+        },
       );
     } catch (error) {
-      this.logger.error(
+      this._logger.error(
         "Error processing kafka even handler  `handleCourseComplete`",
         {
           error,
-        }
+        },
       );
     }
   }
 
   @EventPattern(KafkaTopics.CourseEnrollmentCreated)
   async handleCourseEnrollmentEvent(
-    data: KafkaMessage<CourseEnrollmentEvent>
+    data: KafkaMessage<CourseEnrollmentEvent>,
   ): Promise<void> {
     try {
-      await this.tracer.startActiveSpan(
+      await this._tracer.startActiveSpan(
         "CourseConsumer.handleCourseEnrollmentEvent",
         async (span) => {
-          this.logger.info("Handling `handleCourseEnrollmentEvent` request ", {
-            ctx: CourseConsumer.name,
-          });
-
-          await this.courseEnrolledHandler.handle(
-            data.value
+          this._logger.debug(
+            "Handling `handleCourseEnrollmentEvent` request ",
+            {
+              ctx: CourseConsumer.name,
+            },
           );
 
-          this.logger.info(
-            "handleCourseEnrollmentEvent request has been successfully completed"
+          await this.courseEnrolledHandler.handle(data.value);
+
+          this._logger.debug(
+            "handleCourseEnrollmentEvent request has been successfully completed",
           );
-        }
+        },
       );
     } catch (error) {
-      this.logger.error(
+      this._logger.error(
         "Error processing Kafka handler `handleCourseEnrollmentEvent`",
         {
           error,
-        }
+        },
       );
     }
   }
