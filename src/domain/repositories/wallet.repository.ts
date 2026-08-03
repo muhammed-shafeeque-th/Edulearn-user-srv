51b59d5239f5e6fcd8c5e3f760bbbfb9ddbe2098
shafeeque <shafeeqsha06@gmail.com>
2026-08-03 21:23:28 +0530

    fix: fix domain repository lint issues

--- src/domain/repositories/wallet.repository.ts
+++ src/domain/repositories/wallet.repository.ts
@@ -5,12 +5,12 @@ export abstract class IWalletRepository {
   abstract save(wallet: Wallet): Promise<Wallet>;
   abstract delete(wallet: Wallet): Promise<void>;
   abstract findById(
-    id: string
+    id: string,
   ): Promise<{ wallet: Wallet | null; totalTransactions: number }>;
   abstract findByUserId(
     userId: string,
     offset?: number,
-    limit?: number
+    limit?: number,
   ): Promise<{ wallet: Wallet | null; totalTransactions: number }>;
   abstract update(wallet: Wallet): Promise<void>;
   abstract addTransaction(transaction: WalletTransaction): Promise<void>;
@@ -22,15 +22,15 @@ export abstract class IWalletRepository {
     todayEarnings: number;
   } | null>;
   abstract findTransaction(
-    transactionId: string
+    transactionId: string,
   ): Promise<WalletTransaction | null>;
   abstract findTransactionByWalletIdAndOrderId(
     walletId: string,
-    orderId: string
+    orderId: string,
   ): Promise<WalletTransaction | null>;
   abstract findTransactionsByWalletId(
     walletId: string,
     offset?: number,
-    limit?: number
+    limit?: number,
   ): Promise<{ transactions: WalletTransaction[]; totalTransactions: number }>;
 }
